2e24099938f174bc7a88e0fc3b930d8f03bca89d
Prantik Kundu <Prantik@MH01918006MACLT.local>
2014-07-15 15:27:09 -0400

    alignp_mepi_anat.py -small adjustment to the way cmass is handled

--- meica.libs/alignp_mepi_anat.py
+++ meica.libs/alignp_mepi_anat.py
@@ -119,7 +119,7 @@ def graywt(t2sname,s0name):
 	sl.append("3dcalc -overwrite -a 'Segsy.t2svm/Posterior+orig[2]' -prefix %s -expr 'a' " % weightvol)
 	return basevol,weightvol
 
-def allineate(sourcevol, weight, targetvol, prefix, maxrot, maxshf,maxscl):
+def allineate(sourcevol, weight, targetvol, prefix, maxrot, maxshf,maxscl,do_cmass):
 	"""
 	source should be anatomical
 	target should be T2* 
@@ -127,7 +127,7 @@ def allineate(sourcevol, weight, targetvol, prefix, maxrot, maxshf,maxscl):
 	outvol_prefix = "%s_al"  % prefix
 	outmat_prefix = "%s_al_mat"  % prefix
 	cmass_opt = ''
-	if options.cmass: cmass_opt = '-cmass'
+	if do_cmass: cmass_opt = '-cmass'
 	align_opts = "-lpc -weight %s -maxshf %s -maxrot %s -maxscl %s %s" % (weightvol, maxrot, maxshf,maxscl,cmass_opt)
 	sl.append("3dAllineate -overwrite -weight_frac 1.0 -VERB -warp aff -source_automask+2 -master SOURCE -source %s -base %s -prefix ./%s -1Dmatrix_save ./%s %s " \
 		% (sourcevol,targetvol,outvol_prefix,outmat_prefix,align_opts))
@@ -153,7 +153,7 @@ t2s_name,s0_name,anat_name = import_datasets([options.t2s,options.s0,options.ana
 basevol,weightvol = graywt(t2s_name,s0_name)
 
 """Run 3dAllineate"""
-allin_volume,allin_matrix = allineate(anat_name,weightvol,basevol,options.prefix,options.maxrot,options.maxshf,options.maxscl)
+allin_volume,allin_matrix = allineate(anat_name,weightvol,basevol,options.prefix,options.maxrot,options.maxshf,options.maxscl,options.cmass)
 
 """Run procedure"""
 runproc(options.prefix, sl)
